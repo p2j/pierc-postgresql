a39bdc730e31d3d02101cf17947ed30bbf8e4dab
Peter Jorgensen <ptr.jorgensen@gmail.com>
2014-11-12 21:15:57 -0600

    Simplified systemd scripts, should work now!

--- init_script.sh
+++ init_script.sh
@@ -9,83 +9,19 @@ cd bot
 
 case "$1" in
     start)
-        if [ -e IRCbot.running ]; then
-            if ( kill -0 $(cat IRCbot.running) 2> /dev/null ); then
-                echo "The IRCbot is already running, try restart or stop"
-                exit 1
-            else
-                echo "IRCbot.running found, but no server running..."
-                rm IRCbot.running
-            fi
-        fi
-        if [ "${UID}" = "0" ]; then
-            echo "Don't run the server as root..."
-        fi
-        echo "Starting the IRCbot"
-        python pierc.py > /dev/null &
-        touch IRCbot.running
+        python pierc.py
     ;;
     stop)
-        echo "Stopping the IRCbot..."
-        if ( kill -TERM $(cat IRCbot.running) 2> /dev/null ); then
-            c=1
-            while [ "$c" -le 300 ]; do
-                if ( kill -0 $(cat ts3server.pid) 2> /dev/null ); then
-                    echo -n "."
-                    sleep 1
-                else
-                    break
-                fi
-                c=$(($c+1))
-            done
-            rm IRCbot.running
-        fi
+        pkill -f "python pierc.py"
     ;;
     restart)
-        echo "Stopping the IRCbot..."
-        if ( kill -TERM $(cat IRCbot.running) 2> /dev/null ); then
-            c=1
-            while [ "$c" -le 300 ]; do
-                if ( kill -0 $(cat IRCbot.running) 2> /dev/null ); then
-                    echo -n "."
-                    sleep 1
-                else
-                    break
-                fi
-                c=$(($c+1))
-            done
-            rm IRCbot.running
-        fi
-        if [ -e IRCbot.running ]; then
-            if ( kill -0 $(cat IRCbot.running) 2> /dev/null ); then
-                echo "The IRCbot is already running, try restart or stop"
-                exit 1
-            else
-                echo "IRCbot.running found, but no service running..."
-                rm IRCbot.running
-            fi
-        fi
-        if [ "${UID}" = "0" ]; then
-            echo "Don't run the service as root..."
-        fi
-        echo "Starting the IRCbot"
-        python ./bot/pierc.py
-        touch IRCbot.running
-    ;;
-    status)
-        if [ -e IRCbot.running ]; then
-            if ( kill -0 $(cat IRCbot.running) 2> /dev/null ); then
-                echo "Service is running"
-            else
-                echo "Service has died"
-            fi
-        else
-            echo "No server running (IRCbot.running is missing)"
-        fi
+        pkill -f "python pierc.py"
+        python pierc.py
     ;;
     *)
-        echo "Usage: ${0} {start|stop|restart|status}"
+        echo "Usage: ${0} {start|stop|restart}"
         exit 2
 cd ..
 
+esac
 exit 0
